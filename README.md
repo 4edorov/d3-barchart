98b9c904fcb8803015e9eb3a4f4e8f4c58f0be7d
4edorov <4edorov@gmail.com>
2018-10-23 19:47:24 +0300

    passed tests

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # d3-barchart
-Visualize Data with a Bar Chart: freeCodeCamp project
+
+Visualize Data with a Bar Chart: **freeCodeCamp** project
+
+## Technologies
+
+- HTML
+- CSS
+- JS
+- D3.js
--- scripts.js
+++ scripts.js
@@ -17,12 +17,25 @@ var chart = data => {
   var svg = d3.select('svg')
   var width = 800
   var height = 500
-  var margin = {top: 20, right: 0, bottom: 30, left: 40}
+  var margin = {top: 20, right: 20, bottom: 30, left: 40}
 
-  var x = d3.scaleBand()
-    .domain([new Date(1950, 0, 1), new Date(2015, 0, 1)])
+  var formTimeValues = function(timeString) {
+    times = timeString.split('-')
+    return {
+      year: times[0],
+      month: times[1],
+      day: times[2]
+    }
+  }
+  var startTime = formTimeValues(data[0][0])
+  var endTime = formTimeValues(data[data.length - 1][0])
+
+  var x = d3.scaleTime()
+    .domain([
+      new Date(startTime.year, startTime.month, startTime.day),
+      new Date(endTime.year, endTime.month, endTime.day)
+    ])
     .range([margin.left, width - margin.right])
-    .padding(0.1)
   
   var y = d3.scaleLinear()
     .domain([0, d3.max(data, function(d) { return d[1] })])
@@ -40,18 +53,38 @@ var chart = data => {
             .call(d3.axisLeft(y))
   }
 
+  var tooltip = d3.select('body').append('div')
+    .attr('id', 'tooltip')
+    .attr('class', 'tooltip')
+    .style('opacity', 0)
+
   svg.append('g')
     .attr('fill', 'steelblue')
     .selectAll('rect').data(data).enter().append('rect')
     .attr('class', 'bar')
-    .attr('x', function(d) { return x(d[0]) })
+    .attr('x', function(d) {
+      var time = formTimeValues(d[0])
+      return x(new Date(time.year, time.month, time.day))
+    })
     .attr('y', function(d) { return y(d[1]) })
     .attr('height', function(d) { return (y(0) - y(d[1])) })
     .attr('width', '3')
     .attr('data-date', function(d) { return d[0] })
     .attr('data-gdp', function(d) { return d[1] })
-    .append('title')
-    .text(function(d) { return d })
+    .on('mouseover', function(d) {
+      tooltip.transition()
+        .duration(200)
+        .style('opacity', .9)
+        .attr('data-date', d[0])
+      tooltip.html('Value: $' + d[1] + 'B' + '<br>' + 'Date: ' + d[0])
+        .style('left', (d3.event.pageX + 10) + 'px')
+        .style('top', (d3.event.pageY + 35) + 'px')
+    })
+    .on('mouseout', function(d) {
+      tooltip.transition()
+        .duration(500)
+        .style('opacity', 0)
+    })
 
   svg.append('g')
     .attr('id', 'x-axis')
